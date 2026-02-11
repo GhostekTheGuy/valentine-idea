965fa0488f6d892e464a52a4dc01646145bf5a75
Hubert Kolejko <83242010+GhostekTheGuy@users.noreply.github.com>
2026-02-11 21:13:27 +0100

    ref: xz

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
   },
 };
 
